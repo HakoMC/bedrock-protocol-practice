7887ccb32d6a2809249a9adbc66e1285909adf07
はこ <101483401+HakoMC@users.noreply.github.com>
2024-10-18 20:12:30 +0900

    add console.log

--- client.js
+++ client.js
@@ -6,23 +6,27 @@ const client = bedrock.createClient({
   offline: true, // optional, default false. if true, do not login with Xbox Live. You will not be asked to sign-in if set to true.
 });
 
-client.on("add_player", (packet) => {
-  client.queue(
-    "text",
-    {
+client.on("text", (packet) => {
+  // Listen for chat messages from the server and echo them back.
+  if (packet.source_name != client.username) {
+    console.log("Sending text packet:", {
+      type: packet.type,
+      needs_translation: packet.needs_translation,
+      source_name: packet.source_name,
+      message: packet.message,
+      parameters: packet.parameters,
+      xuid: packet.xuid,
+      platform_chat_id: packet.platform_chat_id,
+      chat_parameters: packet.chat_parameters,
+    });
+    client.queue("text", {
       type: "chat",
       needs_translation: false,
       source_name: client.username,
       xuid: "",
       platform_chat_id: "",
-      message: `Hey, ${packet.username} just joined!`,
-    },
-    (err) => {
-      if (err) {
-        console.error("テキストパケットの送信中にエラーが発生しました:", err);
-      } else {
-        console.log("テキストパケットが正常に送信されました");
-      }
-    },
-  );
+      filtered_message: "",
+      message: `${packet.source_name} said: ${packet.message} on ${new Date().toLocaleString()}`,
+    });
+  }
 });

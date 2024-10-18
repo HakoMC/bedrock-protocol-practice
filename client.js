f4fb3be88d438d4ed91bcbfd77f18672c435290a
はこ <101483401+HakoMC@users.noreply.github.com>
2024-10-18 18:43:54 +0900

    client template all copy

--- client.js
+++ client.js
@@ -8,9 +8,7 @@ const client = bedrock.createClient({
 
 // The 'join' event is emitted after the player has authenticated
 // and is ready to recieve chunks and start game packets
-client.on("join", (client) => {
-  console.log("Player has joined!");
-});
+client.on("join", (client) => console.log("Player has joined!"));
 
 // The 'spawn' event is emitted. The chunks have been sent and all is well.
 client.on("spawn", (client) => console.log("Player has spawned!"));
@@ -18,4 +16,18 @@ client.on("spawn", (client) => console.log("Player has spawned!"));
 // We can listen for text packets. See proto.yml for documentation.
 client.on("text", (packet) => {
   console.log("Client got text packet", packet);
+});
+
+// For example, we can listen to https://prismarinejs.github.io/minecraft-data/?v=bedrock_1.19.60&d=protocol#packet_add_player
+// and send them a chat message when a player joins saying hello. Note the lack of the `packet` prefix, and that the packet
+// names and as explained in the "Protocol doc" section below, fields are all case sensitive!
+client.on("add_player", (packet) => {
+  client.queue("text", {
+    type: "chat",
+    needs_translation: false,
+    source_name: client.username,
+    xuid: "",
+    platform_chat_id: "",
+    message: `Hey, ${packet.username} just joined!`,
+  });
 });

9e56279ad325fd52220cb6af9e2c919a8a9fc16d
はこ <101483401+HakoMC@users.noreply.github.com>
2024-10-19 08:39:51 +0900

    add inventory content packet

--- message.js
+++ message.js
@@ -47,14 +47,14 @@ function openCustomChest(player) {
     runtime_entity_id: -1,
   });
 
-  // player.queue("inventory_content", {
-  //   window_id: 1,
-  //   input: [
-  //     { network_id: 0, count: 1, metadata: 0, block_runtime_id: 0 }, // 空のスロット
-  //     { network_id: 1, count: 64, metadata: 0, block_runtime_id: 0 }, // 石ブロック64個
-  //     // ... 他のアイテムを追加
-  //   ],
-  // });
+  player.queue("inventory_content", {
+    window_id: 1,
+    input: [
+      { network_id: 0, count: 1, metadata: 0, block_runtime_id: 0 }, // 空のスロット
+      { network_id: 1, count: 64, metadata: 0, block_runtime_id: 0 }, // 石ブロック64個
+      // ... 他のアイテムを追加
+    ],
+  });
 }
 
 discordClient.on("ready", () => {

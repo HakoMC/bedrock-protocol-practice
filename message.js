2713e92979f572714f14b8324a0b80d6bb9b312c
はこ <101483401+HakoMC@users.noreply.github.com>
2024-10-18 17:15:39 +0900

    add message.js

---
+++ message.js
@@ -0,0 +1,32 @@
+const { Relay } = require("bedrock-protocol");
+
+const relay = new Relay({
+  version: "1.21.30",
+  host: "100.89.61.11",
+  port: 19132,
+  destination: {
+    host: "127.0.0.1",
+    port: 19131,
+  },
+});
+
+relay.listen();
+
+relay.on("connect", (player) => {
+  console.log("新しい接続:", player.connection.address);
+
+  player.on("join", () => {
+    sendChatMessage(player, "サーバーへようこそ!");
+  });
+});
+
+function sendChatMessage(player, message) {
+  player.queue("text", {
+    type: "chat",
+    needs_translation: false,
+    source_name: "Server",
+    xuid: "",
+    platform_chat_id: "",
+    message: message,
+  });
+}

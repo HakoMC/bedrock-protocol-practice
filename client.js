107a83f246b05f610d8d1ead8f7a5baf2de11dbf
はこ <101483401+HakoMC@users.noreply.github.com>
2024-10-18 20:16:41 +0900

    update queue

--- client.js
+++ client.js
@@ -9,16 +9,6 @@ const client = bedrock.createClient({
 client.on("text", (packet) => {
   // Listen for chat messages from the server and echo them back.
   if (packet.source_name != client.username) {
-    // console.log("Sending text packet:", {
-    //   type: packet.type,
-    //   needs_translation: packet.needs_translation,
-    //   source_name: packet.source_name,
-    //   message: packet.message,
-    //   parameters: packet.parameters,
-    //   xuid: packet.xuid,
-    //   platform_chat_id: packet.platform_chat_id,
-    //   chat_parameters: packet.chat_parameters,
-    // });
     client.queue("text", {
       type: "chat",
       needs_translation: false,

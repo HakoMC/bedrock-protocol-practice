5361cd0d0a8d9574ab109acdbdf6b40aba06b6cb
はこ <101483401+HakoMC@users.noreply.github.com>
2024-10-18 21:11:24 +0900

    discord test

--- message.js
+++ message.js
@@ -1,4 +1,14 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
 const { Relay } = require("bedrock-protocol");
+
+const discordClient = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
 const relay = new Relay({
   version: "1.21.30",
@@ -13,9 +23,38 @@ const relay = new Relay({
 relay.conLog = console.debug;
 relay.listen();
 
+let minecraftPlayer = null;
+
 relay.on("connect", (player) => {
-  console.log("新しい接続:", player.connection.address);
-  player.on("join", () => {
-    console.log("Player profile:", player.profile);
-  });
+  console.log("Minecraft接続確立:", player.connection.address);
+  minecraftPlayer = player;
 });
+
+discordClient.on("ready", () => {
+  console.log(`Logged in as ${discordClient.user.tag}!`);
+});
+
+discordClient.on("messageCreate", (message) => {
+  if (message.author.bot) return; // ボットのメッセージは無視
+
+  console.log(
+    `Discord message: ${message.author.username}: ${message.content}`,
+  );
+
+  if (minecraftPlayer) {
+    minecraftPlayer.queue("text", {
+      type: "chat",
+      needs_translation: false,
+      source_name: message.author.username,
+      xuid: "",
+      platform_chat_id: "",
+      filtered_message: "",
+      message: message.content,
+    });
+    console.log("Message sent to Minecraft");
+  } else {
+    console.log("Minecraft player not connected");
+  }
+});
+
+discordClient.login(process.env.DISCORD_TOKEN);

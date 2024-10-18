fb56bc3b348ba264dff8231f60aace9d8927005c
はこ <101483401+HakoMC@users.noreply.github.com>
2024-10-18 20:19:18 +0900

    update queue

--- client.js
+++ client.js
@@ -6,17 +6,13 @@ const client = bedrock.createClient({
   offline: true, // optional, default false. if true, do not login with Xbox Live. You will not be asked to sign-in if set to true.
 });
 
-client.on("text", (packet) => {
-  // Listen for chat messages from the server and echo them back.
-  if (packet.source_name != client.username) {
-    client.queue("text", {
-      type: "chat",
-      needs_translation: false,
-      source_name: client.username,
-      xuid: "",
-      platform_chat_id: "",
-      filtered_message: "",
-      message: `${packet.source_name} said: ${packet.message} on ${new Date().toLocaleString()}`,
-    });
-  }
+client.on("add_player", (packet) => {
+  client.queue("text", {
+    type: "chat",
+    needs_translation: false,
+    source_name: client.username,
+    xuid: "",
+    platform_chat_id: "",
+    message: `Hey, ${packet.username} just joined!`,
+  });
 });

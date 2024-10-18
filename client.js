3e07b7a826d98f002e7c2a582f5a18a18a95b06a
はこ <101483401+HakoMC@users.noreply.github.com>
2024-10-18 19:20:34 +0900

    add error handring

--- client.js
+++ client.js
@@ -6,21 +6,23 @@ const client = bedrock.createClient({
   offline: true, // optional, default false. if true, do not login with Xbox Live. You will not be asked to sign-in if set to true.
 });
 
-player.queue(
-  "text",
-  {
-    type: "chat",
-    needs_translation: false,
-    source_name: client.username,
-    xuid: "",
-    platform_chat_id: "",
-    message: `Hey, ${packet.username} just joined!`,
-  },
-  (err) => {
-    if (err) {
-      console.error("テキストパケットの送信中にエラーが発生しました:", err);
-    } else {
-      console.log("テキストパケットが正常に送信されました");
-    }
-  },
-);
+client.on("add_player", (packet) => {
+  client.queue(
+    "text",
+    {
+      type: "chat",
+      needs_translation: false,
+      source_name: client.username,
+      xuid: "",
+      platform_chat_id: "",
+      message: `Hey, ${packet.username} just joined!`,
+    },
+    (err) => {
+      if (err) {
+        console.error("テキストパケットの送信中にエラーが発生しました:", err);
+      } else {
+        console.log("テキストパケットが正常に送信されました");
+      }
+    },
+  );
+});

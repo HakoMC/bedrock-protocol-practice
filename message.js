80a108a9ea388508b5f4c81dc7937d989c69c3d7
はこ <101483401+HakoMC@users.noreply.github.com>
2024-10-18 20:34:10 +0900

    update queue

--- message.js
+++ message.js
@@ -15,16 +15,15 @@ relay.listen();
 
 relay.on("connect", (player) => {
   console.log("新しい接続:", player.connection.address);
-
+  player.queue("text", {
+    type: "system",
+    needs_translation: false,
+    xuid: "",
+    platform_chat_id: "",
+    filtered_message: "",
+    message: `Someone just joined!`,
+  });
   player.on("join", () => {
     console.log("Player profile:", player.profile);
-    player.queue("text", {
-      type: "system",
-      needs_translation: false,
-      xuid: "",
-      platform_chat_id: "",
-      filtered_message: "",
-      message: `Someone just joined!`,
-    });
   });
 });

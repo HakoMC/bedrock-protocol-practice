bcfe2027cca0bf62ecb1df20dd710c9c53690674
はこ <101483401+HakoMC@users.noreply.github.com>
2024-10-19 09:17:38 +0900

    fix inventory content packet

--- message.js
+++ message.js
@@ -47,38 +47,38 @@ function openCustomChest(player) {
     runtime_entity_id: -1n,
   });
 
-  player.queue("inventory_content", {
-    window_id: 1,
-    content: [
-      {
-        network_id: 0,
-        count: 0,
-        metadata: 0,
-        has_stack_id: 0,
-        stack_id: 0,
-        block_runtime_id: 0,
-        extra: {
-          has_nbt: 0,
-          can_place_on: [],
-          can_destroy: [],
-        },
-      },
-      {
-        network_id: 1,
-        count: 64,
-        metadata: 0,
-        has_stack_id: 1,
-        stack_id: 1,
-        block_runtime_id: 0,
-        extra: {
-          has_nbt: 0,
-          can_place_on: [],
-          can_destroy: [],
-        },
-      },
-      // 他のスロットも同様に定義...
-    ],
-  });
+  // player.queue("inventory_content", {
+  //   window_id: 1,
+  //   content: [
+  //     {
+  //       network_id: 0,
+  //       count: 0,
+  //       metadata: 0,
+  //       has_stack_id: 0,
+  //       stack_id: 0,
+  //       block_runtime_id: 0,
+  //       extra: {
+  //         has_nbt: 0,
+  //         can_place_on: [],
+  //         can_destroy: [],
+  //       },
+  //     },
+  //     {
+  //       network_id: 1,
+  //       count: 64,
+  //       metadata: 0,
+  //       has_stack_id: 1,
+  //       stack_id: 1,
+  //       block_runtime_id: 0,
+  //       extra: {
+  //         has_nbt: 0,
+  //         can_place_on: [],
+  //         can_destroy: [],
+  //       },
+  //     },
+  //     // 他のスロットも同様に定義...
+  //   ],
+  // });
 }
 
 discordClient.on("ready", () => {
